d2bf0404300ef3df14aa220487b95cc53de6cf48
sweetybarnawal20 <sweetybarnawal20@gmail.com>
2026-07-29 18:51:33 +0545

    Implemented custom user model and initial migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        CANDIDATE = "CANDIDATE", "Candidate"
+        EMPLOYER = "EMPLOYER", "Employer"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CANDIDATE,
+    )
