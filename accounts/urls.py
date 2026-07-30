f180b5ef084662bca70dc851d51629fff35035bd
sweetybarnawal20 <sweetybarnawal20@gmail.com>
2026-07-30 13:59:16 +0545

    Added Bootstrap home page and navigation

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
