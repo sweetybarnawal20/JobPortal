630a47a95a6f9029471d158ef8b12e9c443bab69
sweetybarnawal20 <sweetybarnawal20@gmail.com>
2026-07-30 14:34:36 +0545

    Implemented user registration functionality

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import User
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+
+        fields = (
+            "username",
+            "email",
+            "role",
+            "password1",
+            "password2",
+        )
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def home(request):
@@ -8,4 +9,25 @@ def login_view(request):
     return render(request, 'accounts/login.html')
 
 def register_view(request):
-    return render(request, 'accounts/register.html')
+
+    if request.method == "POST":
+
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("login")
+
+    else:
+
+        form = UserRegistrationForm()
+
+    return render(
+        request,
+        "accounts/register.html",
+        {
+            "form": form
+        },
+    )
