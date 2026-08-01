483f57f46cbb5ab768e24cbb52c868f0e0ab5e35
sweetybarnawal20 <sweetybarnawal20@gmail.com>
2026-08-01 14:41:46 +0545

    Added pillow dependency and CandidateProfile Model

--- accounts/models.py
+++ accounts/models.py
@@ -13,3 +13,51 @@ class User(AbstractUser):
         choices=Role.choices,
         default=Role.CANDIDATE,
     )
+    
+class CandidateProfile(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+
+    address = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+
+    bio = models.TextField(
+        blank=True,
+    )
+
+    skills = models.TextField(
+        blank=True,
+    )
+
+    education = models.TextField(
+        blank=True,
+    )
+
+    experience = models.TextField(
+        blank=True,
+    )
+
+    resume = models.FileField(
+        upload_to="resumes/",
+        blank=True,
+        null=True,
+    )
+
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.user.username    
